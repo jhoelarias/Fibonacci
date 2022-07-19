36b4ef290924fa3c6786a663fd627b1e4952607c
Jhoel Arias <jarias@arrivelogistics.com>
2022-07-19 08:52:34 -0500

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Fibonacci;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Fibonacci().Take(10).ToList().ForEach(s => Console.WriteLine(s)); ;
+    }
+
+    private static IEnumerable<double> Fibonacci()
+    {
+        var series = new List<double>() { 0, 1 };
+        yield return series[0];
+        yield return series[1];
+        while (true)
+        {
+            var current = series[^2] + series[^1];
+            series.Add(current);
+            yield return current;
+        }
+    }
+}
